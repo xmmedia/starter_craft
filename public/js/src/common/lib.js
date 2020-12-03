aa3e7fdab3368982edb50afa794b1e23b7d62b45
Darryl Hein <dhein@xmmedia.com>
2020-12-03 10:44:39 -0700

    cleanup lib.js

--- public/js/src/common/lib.js
+++ public/js/src/common/lib.js
@@ -1,15 +1,12 @@
 export const logError = function (e) {
     if (console && e !== undefined) {
+        // eslint-disable-next-line no-console
         console.error(e);
     }
 };
 
 export const hasGraphQlError = function (e) {
     return e && e.graphQLErrors && e.graphQLErrors[0];
-};
-
-export const hasGraphQlValidationError = function (e) {
-    return e && e.graphQLErrors && e.graphQLErrors[0] && e.graphQLErrors[0].validation;
 };
 
 export const waitForValidation = function () {
