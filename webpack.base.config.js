b94f5ca920f92bbec06bb7a0b104b375ba64275d
Darryl Hein <dhein@xmmedia.com>
2021-01-14 13:44:10 -0700

    upgrade webpack encore

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -16,7 +16,7 @@ module.exports = function (Encore) {
     }
 
     Encore
-    // directory where all compiled assets will be stored
+        // directory where all compiled assets will be stored
         .setOutputPath('public/build/')
 
         // what's the public path to this directory (relative to your project's document root dir)
@@ -39,7 +39,7 @@ module.exports = function (Encore) {
         .enableIntegrityHashes(Encore.isProduction())
 
         // allow sass/scss files to be processed
-        .enableSassLoader(function() {}, {
+        .enableSassLoader(function () {}, {
             // see: http://symfony.com/doc/current/frontend/encore/bootstrap.html#importing-bootstrap-sass
             resolveUrlLoader: false,
         })
@@ -53,7 +53,7 @@ module.exports = function (Encore) {
                     dangerousTaggedTemplateString: true,
                 },
             };
-        })
+        }, { runtimeCompilerBuild: true })
 
         // generate source maps when "source-maps" argument exists
         .enableSourceMaps(
@@ -103,6 +103,15 @@ module.exports = function (Encore) {
         .addPlugin(new Dotenv({
             path: './.env',
         }))
+
+        // this is to resolve the issues with the manifest
+        // where the file path keys have the hashed version
+        .configureUrlLoader({
+            images: {
+                limit: 0, // Avoids files from being inlined
+                esModule: false,
+            },
+        })
     ;
 
     if (Encore.isProduction()) {
