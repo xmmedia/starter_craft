83982a4ecf10e054906ff67b781178323a412b32
Herman Steuernagel <herman@xmmedia.com>
2024-12-05 13:58:04 -0800

    vite php cleanup

--- config/vite.php
+++ config/vite.php
@@ -6,15 +6,11 @@ return [
     // Path to the Vite manifest file, relative to the Craft base path.
     'manifestPath' => '@webroot/build/.vite/manifest.json',
 
-    // URL prefix for assets, should match the `base` in `vite.config.mjs`.
-    'assetBasePath' => '/build/',
-
     // Dynamically enable dev server based on environment.
     'useDevServer' => App::env('ENVIRONMENT') === 'dev' || App::env('CRAFT_ENVIRONMENT') === 'dev',
 
     // URL to the Vite development server.
-    'devServerPublic' => 'https://localhost:9028/',
-
+    'devServerPublic' => 'https://localhost:9028/build/',
     'checkDevServer' => false,
 
     // Public URL for production assets.
@@ -22,8 +18,6 @@ return [
 
     // Content-Security-Policy hash for inline tags.
     'includeIntegrityHashes' => true,
-
     'includeModulePreloadShim' => false,
-
     'includeScriptOnloadHandler' => false,
 ];
