725e86507c787b198e15e5c2622a38b15f1d8093
Herman Steuernagel <herman@xmmedia.com>
2025-06-09 14:08:08 -0700

    adding postcss nested - so css acts more like sass

---
+++ postcss.config.cjs
@@ -0,0 +1,6 @@
+export default {
+    plugins: {
+        'postcss-nested': {},
+        // PostCSS is handled by tailwind, so we don't need to include it here, but the nested plugin is not
+    },
+}
