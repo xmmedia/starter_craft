9d278120953deb26a70125b132afd14dbe810071
Herman Steuernagel <herman@xmmedia.com>
2025-06-09 14:16:11 -0700

    remove psotcss nested (didn’t work as expected)

--- postcss.config.cjs
+++
@@ -1,6 +0,0 @@
-export default {
-    plugins: {
-        'postcss-nested': {},
-        // PostCSS is handled by tailwind, so we don't need to include it here, but the nested plugin is not
-    },
-}
