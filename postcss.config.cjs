c14f70d08c251e47c131ef480e3b5d7febbe8ef7
Herman Steuernagel <herman@xmmedia.com>
2025-06-09 10:53:56 -0700

    remove postcss files

--- postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-    plugins: [
-        require('@tailwindcss/postcss'),
-    ],
-    sourceMap : 'prev',
-};
-
-module.exports = config;
