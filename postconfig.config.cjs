7d18e2151991df1fde8cf8cf81b12aec3af61fd0
Herman Steuernagel <herman@xmmedia.com>
2025-06-11 14:10:51 -0700

    fix and rename postconfig

--- postconfig.config.cjs
+++ postconfig.config.cjs
@@ -1,7 +1,11 @@
-export default {
+/** @type {import('postcss-load-config').Config} */
+const config = {
     plugins: {
         'postcss-env-function': {},
         'postcss-nesting': {},
         // PostCSS is handled by tailwind, so we don't need to include it here, but the nested and env-function plugins are not
     },
-}
+    sourceMap : 'prev',
+};
+
+module.exports = config;
