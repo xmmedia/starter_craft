663b00c33bbdc245e57bee76676d5c78c6ae6b26
Darryl Hein <dhein@xmmedia.com>
2025-11-18 12:17:43 -0700

    ws

--- config/vite.php
+++ config/vite.php
@@ -4,21 +4,21 @@ use craft\helpers\App;
 
 return [
     // Path to the Vite manifest file, relative to the Craft base path.
-    'manifestPath' => '@webroot/build/.vite/manifest.json',
+    'manifestPath'               => '@webroot/build/.vite/manifest.json',
 
     // Dynamically enable dev server based on environment.
-    'useDevServer' => App::env('ENVIRONMENT') === 'dev' || App::env('CRAFT_ENVIRONMENT') === 'dev',
+    'useDevServer'               => App::env('ENVIRONMENT') === 'dev' || App::env('CRAFT_ENVIRONMENT') === 'dev',
 
     // URL to the Vite development server.
-//    @todo update this to the correct port (from vite.config.js)
-    'devServerPublic' => 'https://localhost:9028/build/',
-    'checkDevServer' => false,
+    // @todo update this to the correct port (from vite.config.js)
+    'devServerPublic'            => 'https://localhost:9028/build/',
+    'checkDevServer'             => false,
 
     // Public URL for production assets.
-    'serverPublic' => '/build/',
+    'serverPublic'               => '/build/',
 
     // Content-Security-Policy hash for inline tags.
-    'includeIntegrityHashes' => true,
-    'includeModulePreloadShim' => false,
+    'includeIntegrityHashes'     => true,
+    'includeModulePreloadShim'   => false,
     'includeScriptOnloadHandler' => false,
 ];
