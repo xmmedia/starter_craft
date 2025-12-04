700f270a2b9a778ef8f7cc772ad0620779815b15
Darryl Hein <dhein@xmmedia.com>
2025-12-03 20:24:38 -0700

    cleaning up root JS file

--- public/js/src/public.js
+++ public/js/src/public.js
@@ -1,20 +1,12 @@
 import { createApp } from 'vue';
+
+// components
 import Menu from './common/menu.vue';
-
-// import * as filters from './common/filters';
-
-// import formError from './common/form_error';
-// import fieldErrors from './common/field_errors';
-// import fieldError from './common/field_error';
 
 // CSS
 import '../../css/public.css';
 
-// images
+// icons
 import '../../images/icons-public.svg';
-
-// createApp(formError).mount('#form-error');
-// createApp(fieldErrors).mount('#field-errors');
-// createApp(fieldError).mount('#field-error');
 
 createApp(Menu).mount('#menu');
