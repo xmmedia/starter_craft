8b3b3b4592a9e1910ee00d4a525c710c15d2acee
Darryl Hein <dhein@xmmedia.com>
2026-03-02 16:23:37 -0700

    move w-content & transition-default into the TWv4 @utility
    create new config partial

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,5 @@
-const plugin = require('tailwindcss/plugin');
-
 module.exports = {
     plugins: [
         require('@tailwindcss/typography'),
-
-        plugin(({ addComponents, theme }) => {
-            addComponents({
-                /* goes into the `components` layer, so @apply can see it */
-                '.transition-default': {
-                    transitionProperty:   theme('transitionProperty.all'),
-                    transitionDuration:   theme('transitionDuration.300'),
-                    transitionTimingFunction: theme('transitionTimingFunction.in-out'),
-                },
-            });
-        }),
     ],
 };
