437001d8a05d733ad7d9573d974c9b71d7cfce4c
Darryl Hein <dhein@xmmedia.com>
2026-04-29 14:15:09 -0600

    set base when in prod

--- vite.config.mjs
+++ vite.config.mjs
@@ -14,6 +14,7 @@ export default defineConfig(({ command }) => {
             vuePlugin(),
             tailwindcss(),
         ],
+        base: command === 'build' ? '/build/' : '/',
         build: {
             outDir: 'public/build',
             rolldownOptions: {
