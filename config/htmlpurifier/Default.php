f6c92b7b1bd5ef8d6824c37c44345a0f33b8e697
Darryl Hein <dhein@xmmedia.com>
2026-07-13 21:53:43 -0600

    add support for soft/shy hyphens

--- config/htmlpurifier/Default.php
+++ config/htmlpurifier/Default.php
@@ -1,11 +1,28 @@
 <?php
 
 use HTMLPurifier_Config;
+use HTMLPurifier_Filter;
 
-return static function (HTMLPurifier_Config $config) {
+// Preserve soft hyphens (&shy;) which HTMLPurifier strips by default
+$softHyphenFilter = new class () extends HTMLPurifier_Filter {
+    public $name = 'SoftHyphen';
+
+    public function preFilter($html, $config, $context): string
+    {
+        return str_replace("\xc2\xad", '[[SHY]]', $html);
+    }
+
+    public function postFilter($html, $config, $context): string
+    {
+        return str_replace('[[SHY]]', "\xc2\xad", $html);
+    }
+};
+
+return static function (HTMLPurifier_Config $config) use ($softHyphenFilter) {
     $config->set('Attr.AllowedFrameTargets', ['_blank']);
     $config->set('Attr.EnableID', true);
     $config->set('HTML.AllowedComments', ['pagebreak']);
     $config->set('HTML.SafeIframe', true);
     $config->set('URI.SafeIframeRegexp', '%^(https?:)?//(www.youtube.com/embed/|player.vimeo.com/video/)%');
+    $config->set('Filter.Custom', [$softHyphenFilter]);
 };
