f80292a3dda2721ad0281d53661a930cde7cbcba
Darryl Hein <dhein@xmmedia.com>
2026-07-15 16:57:53 -0600

    switch to ajax contact form

--- public/js/src/public.js
+++ public/js/src/public.js
@@ -3,6 +3,8 @@ import { createApp } from 'vue';
 // components
 import Menu from './common/menu.vue';
 
+import { initAjaxForms } from './common/ajax_form';
+
 // CSS
 import '../../css/public.css';
 
@@ -11,3 +13,5 @@ import '../../images/icons-public.svg';
 
 const menuEl = document.getElementById('menu');
 createApp(Menu, { ...menuEl.dataset }).mount(menuEl);
+
+initAjaxForms();
